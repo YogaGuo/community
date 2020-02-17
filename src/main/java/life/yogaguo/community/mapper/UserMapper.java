e8cf4755504308296c5162917336d6accb83b747
YogaGuo <3376383434@qq.com>
2020-02-17 19:24:26 +0800

    实现持久化登录

--- src/main/java/life/yogaguo/community/mapper/UserMapper.java
+++ src/main/java/life/yogaguo/community/mapper/UserMapper.java
@@ -3,6 +3,8 @@ package life.yogaguo.community.mapper;
 import life.yogaguo.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -15,4 +17,12 @@ public  interface UserMapper {
     @Insert("insert into user(name,account_id,token,gmt_create,gmt_modified) " +
             "value(#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void saveUser(User user);
+
+    /**
+     *
+     * @param token
+     * @return
+     */
+    @Select("select * from user where token=#{token}")
+    User findByToken(@Param("token") String token);
 }
